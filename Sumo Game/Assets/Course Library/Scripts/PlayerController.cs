fed3f857e86327a218758986df7ee0d8e71d2701
mmalcolm02 <mmalcolm02@qub.ac.uk>
2023-10-20 22:49:10 +0100

    Powerup
    Powerup functionality added

--- Sumo Game/Assets/Course Library/Scripts/PlayerController.cs
+++ Sumo Game/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     private Rigidbody playerRB;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,35 @@ public class PlayerController : MonoBehaviour
 
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.62f, 0);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCooldown());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 enemyDirection = (collision.gameObject.transform.position - transform.position);
+            Debug.Log("Collided with " + collision.gameObject.name + "with power up set to" + hasPowerup);
+            enemyRigidbody.AddForce(enemyDirection * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
